f442823a10e8d4a7e1aee59f726c96578a7d0a13
Emily Haynes <ehayne00@hotmail.com>
2019-10-30 17:18:02 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,7 +16,7 @@ def literal_aoh
   new_array = [
     {name:  "Don Gately", occupation: "Live-in Staff"},
     {name:  "Joelle van Dyne", occupation: "Radio Personality"},
-    {},
+    {name:  "Pat Monteseian", occupation: "Staff"},
     {},
     {}
     ]
